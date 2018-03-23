6f1d1842c9cf4c65c01d14a1e985ca61be79fabe
FrostByte616 <tj.bowles666@gmail.com>
2018-03-23 22:16:50 +0000

    Initial push
    project start, main menu and tilt menu scenes.

---
+++ cs4084_project/app/src/main/java/ie/ul/csis/cs4084_project/TiltMenu.java
@@ -0,0 +1,15 @@
+package ie.ul.csis.cs4084_project;
+
+import android.os.Bundle;
+
+/**
+ * Created by tjbow on 21/03/2018.
+ */
+
+public class TiltMenu extends MainMenu {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+}
